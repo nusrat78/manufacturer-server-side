959240e65f3d6110224fb9f1388c6c6e63ea68fd
nusrat78 <njahan5061@gmail.com>
2022-05-26 13:30:54 +0600

    verify jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -13,11 +14,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('engineer-reliance').collection('tools');
         const purchasingCollection = client.db('engineer-reliance').collection('purchasing');
+        const userCollection = client.db('engineer-reliance').collection('users');
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -32,6 +49,21 @@ async function run() {
             res.send(tool);
         })
 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            console.log(process.env.ACCESS_TOKEN_SECRET)
+            res.send({ result, token });
+
+        })
+
         app.get('/available', async (req, res) => {
             // const toolName = req.query.toolName;
             const tools = await toolCollection.find().toArray();
@@ -41,12 +73,18 @@ async function run() {
 
         })
 
-        app.get('/purchasing', async (req, res) => {
+        app.get('/purchasing', verifyJWT, async (req, res) => {
             const buyer = req.query.buyer;
-            const query = { buyer: buyer };
-            const purchasing = await purchasingCollection.find().toArray();
-            res.send(purchasing);
+            const decodedEmail = req.decoded.email;
+            if (buyer === decodedEmail) {
+                const query = { buyer: buyer };
+                const purchasing = await purchasingCollection.find().toArray();
+                return res.send(purchasing);
 
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
         })
         app.post('/purchasing', async (req, res) => {
             const purchasing = req.body;
